8daf88bffee9bb9222ebadd4e646916cb21bb358
alexkrewson <alex.krewson@gmail.com>
2022-07-23 12:37:58 -0700

    added project list and about me text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import TodoList from './TodoList'
 import '../src/sassystyle.scss';
 import React, { useState, useRef, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import me from './me.jpg';
+import me from './me2.jpg';
 
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
@@ -53,24 +53,42 @@ function App() {
       <button onClick={handleClearTodos}>Clear Complete</button>
       <div>{todos.filter(todo => !todo.complete).length}</div> */}
       <div className="gridContainer">
+        {/* <div className="header"></div> */}
         <div className="sidebar">
-            <img src={me} alt="" />
-            <a href="#about">about</a>
-            <a href="#projects">projects</a>
-            <a href="#ghMetrics">github metrics</a>
-            <a href="#resume">resume</a>
+          <img src={me} alt="" />
+          <a href="#about">about</a>
+          <a href="#projects">projects</a>
+          <a href="#ghMetrics">github metrics</a>
+          <a href="#resume">resume</a>
           {/* <div className="sidebarFixed">
           </div> */}
         </div>
         <div className="content">
           <div className="contentSections" id="about">
             <h1>About Me</h1>
-            Please hire me I want your monies! Please hire me I want your monies! Please hire me I want your monies! Please hire me I want your monies! Please hire me I want your monies! Please hire me I want your monies! Please hire me I want your monies! Please hire me I want your monies!
-            
+            I am seeking a path to combine my years of controls and hydraulic engineering experience with my nascent skillset as a front end developer. After completing an online full-stack boot-camp (The Odin Project), I am eager to put my skills to use in the real world and to gain experience by working with other programmers.
+
           </div>
           <div className="contentSections" id="projects">
             <h1>Projects</h1>
-            Ad inventore distinctio aut quibusdam eveniet rem dignissimos doloribus qui voluptatum asperiores in corrupti libero vel quia optio. Ut optio asperiores est perspiciatis dignissimos ad architecto dolor. Ad incidunt aspernatur et ullam dolores eum labore dicta.
+            {/* Ad inventore distinctio aut quibusdam eveniet rem dignissimos doloribus qui voluptatum asperiores in corrupti libero vel quia optio. Ut optio asperiores est perspiciatis dignissimos ad architecto dolor. Ad incidunt aspernatur et ullam dolores eum labore dicta. */}
+            <div className='project-row'>
+              <p>Weather App</p>
+              <a href="https://alexkrewson.github.io/weather/dist/index.html" className='button' target="_blank">live preview</a>
+              <a href="https://github.com/alexkrewson/portfolio/tree/main/weather" className='button' target="_blank">view code</a>
+            </div>
+
+            <div className='project-row'>
+              <p>Todo List</p>
+              <a href="https://alexkrewson.github.io/todolist/dist/index.html" className='button' target="_blank">live preview</a>
+              <a href="https://github.com/alexkrewson/portfolio/tree/main/todo" className='button' target="_blank">view code</a>
+            </div>
+            <div className='project-row'>
+              <p>Weather App</p>
+              <a href="https://arcane-inlet-56598.herokuapp.com/" className='button' target="_blank">live preview</a>
+              <a href="https://github.com/alexkrewson/portfolio/tree/main/fb" className='button' target="_blank">view code</a>
+            </div>
+
           </div>
           <div className="contentSections" id="ghMetrics">
             <h1>Github Metrics</h1>
