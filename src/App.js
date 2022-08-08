d95badc46fc288c4526af4ae30b52c0437469769
alexkrewson <alex.krewson@gmail.com>
2022-08-07 22:32:16 -0700

    github metrics work

--- src/App.js
+++ src/App.js
@@ -11,58 +11,54 @@ import resumePDF from './Resume.pdf';
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
+  // const [todos, setTodos] = useState([])
+  // const todoNameRef = useRef()
 
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos(storedTodos)
-  }, [])
+  // useEffect(() => {
+  //   const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  //   if (storedTodos) setTodos(storedTodos)
+  // }, [])
 
-  useEffect(() => {
-    // console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)))
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-    // console.log('working?')
-  }, [todos])
+  // useEffect(() => {
+  //   // console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)))
+  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  //   // console.log('working?')
+  // }, [todos])
 
-  function toggleTodo(id) {
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
-  }
+  // function toggleTodo(id) {
+  //   const newTodos = [...todos]
+  //   const todo = newTodos.find(todo => todo.id === id)
+  //   todo.complete = !todo.complete
+  //   setTodos(newTodos)
+  // }
 
-  function handleAddTodo(e) {
-    const name = todoNameRef.current.value
-    if (name === '') return
-    setTodos(prevTodos => {
-      console.log('setTodos is working')
-      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
-    })
-    todoNameRef.current.value = null
-  }
+  // function handleAddTodo(e) {
+  //   const name = todoNameRef.current.value
+  //   if (name === '') return
+  //   setTodos(prevTodos => {
+  //     console.log('setTodos is working')
+  //     return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+  //   })
+  //   todoNameRef.current.value = null
+  // }
 
-  function handleClearTodos() {
-    const newTodos = todos.filter(todo => !todo.complete)
-    setTodos(newTodos)
-  }
+  // function handleClearTodos() {
+  //   const newTodos = todos.filter(todo => !todo.complete)
+  //   setTodos(newTodos)
+  // }
 
   const [thisIsMyCopy, setThisIsMyCopy] = useState({})
 
 
   
   useEffect (() => {
+
     async function fetchData() {
-
       const thisIsMyCopy2 = await LoadJSON()
-
-      setThisIsMyCopy(thisIsMyCopy2)  
-
-      // console.log(thisIsMyCopy2)
+      setThisIsMyCopy(thisIsMyCopy2)
     }
+    
     fetchData()
-
-    // console.log(thing)
   }, [])
 
 
--- src/LoadJSON.js
+++ src/LoadJSON.js
@@ -3,12 +3,65 @@ import React from 'react'
 
 
 export default async function loadJSON() {
-    const statsUrl = 'https://api.github.com/repos/alexkrewson/re-former/languages';
-    let response = await fetch(statsUrl);
+    // const statsUrl = 'https://api.github.com/repos/alexkrewson/todo/languages';
+    const statsUrl = 'https://api.github.com/users/alexkrewson/repos?per_page=100';
+
+    const s = {
+        withCredentials: !0,
+        headers: {
+          Authorization: 'token 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'
+        }
+      };
+
+    let response = await fetch(statsUrl,s);
     let data = await response.text();
     let obj = await JSON.parse(data)
-    // console.log(obj.Ruby);
-    return obj
+
+    console.log(obj)
+
+    let obj2 = []
+    let languageTallies = {Javascript: 0, Ruby: 0, HTML: 0, CSS: 0, Sass: 0}
+
+    for (let i = 0; i < obj.length; i++) {
+        let languageUrl = obj[i].languages_url
+
+        let response2 = await fetch(languageUrl,s);
+        let data2 = await response2.text();
+        obj2[i] = await JSON.parse(data2);
+
+        if (obj2[i].JavaScript > 0) {
+            languageTallies.Javascript ++
+        }
+        if (obj2[i].Ruby > 0) {
+            languageTallies.Ruby ++
+        }
+        if (obj2[i].HTML > 0) {
+            languageTallies.HTML ++
+        }
+        if (obj2[i].CSS > 0) {
+            languageTallies.CSS ++
+        }
+        if (obj2[i].SCSS > 0) {
+            languageTallies.Sass ++
+        }
+
+
+    }
+    console.log(obj2)
+    console.log(languageTallies)
+
+    // let otherObj = obj[3].languages_url;
+    // console.log(otherObj);
+
+
+
+
+
+
+    // console.log(obj);
+
+
+    return languageTallies
 
 
 }
